b27c1c023291b7f678c7c744431793b14a2180d5
ander-gil <63676480+ander-gil@users.noreply.github.com>
2024-06-11 20:26:26 -0500

    empezando pryecto jpa basico

---
+++ src/test/java/net/ander/projects/IntroSpringDataJpa/IntroSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ander.projects.IntroSpringDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
